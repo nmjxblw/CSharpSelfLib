db0c21880b3c67f2a863e32ea1dac2abd14d92cd
Octette <915253492@qq.com>
2025-03-05 17:04:34 +0800

    添加运行时环境安装软件

---
+++ Dopamine/DataBase/Access/AccessManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dopamine
+{
+	/// <summary>
+	/// Access管理器
+	/// </summary>
+	public class AccessManager
+	{
+		/// <summary>
+		/// 延迟实例化
+		/// </summary>
+		private static Lazy<AccessManager> _lazy = new Lazy<AccessManager>(() => new AccessManager(), LazyThreadSafetyMode.ExecutionAndPublication);
+		private AccessManager() { }
+		/// <summary>
+		/// 单例实例化
+		/// </summary>
+		public static AccessManager Instance => _lazy.Value;
+	}
+}
