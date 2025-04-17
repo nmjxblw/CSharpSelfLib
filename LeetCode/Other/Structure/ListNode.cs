89deedbe0f9f58122efc20f48f0c7936df1f3122
Octette <915253492@qq.com>
2025-04-17 19:05:52 +0800

    leetcode workspace

---
+++ LeetCode/Other/Structure/ListNode.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class ListNode
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		public int val;
+		/// <summary>
+		/// 
+		/// </summary>
+		public ListNode next;
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="val"></param>
+		/// <param name="next"></param>
+		public ListNode(int val = 0, ListNode next = null)
+		{
+			this.val = val;
+			this.next = next;
+		}
+	}
+}
