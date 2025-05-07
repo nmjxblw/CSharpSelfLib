97115cccc1f182672928e2da6731e7b2118e1dd3
Octette <915253492@qq.com>
2025-05-07 19:03:49 +0800

    merge

--- CompanyCode/CompanyApp.cs
+++ CompanyCode/CompanyApp.cs
@@ -1,4 +1,6 @@
-﻿using System.Diagnostics;
+﻿using Microsoft.AspNetCore.Mvc.Internal;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 
 namespace CompanyCode
 {
@@ -12,14 +14,31 @@ namespace CompanyCode
 		/// </summary>
 		public void Start()
 		{
-			string id = "34 32 B3 35";
-			string pwdAndUser = "33 33 33 33 33 33 33 33";
-			byte phase = 0x33;
-			byte method = 0x34;
-			byte type = 0x33;
-			string frameString = "54 BF 52 33 33 33 33 33 33 33 33 33 33 F6 CC 33 33 33 33 33 33 33 33 33 DA 2E 75 33 33 33 33 33 33 33 33 33 33 33 33 33 33 33 33 33 33 33 33 33 33 33 33 33 33 33 33 33 33 33 33";
-			int framStringCount = frameString.Split(" ").Count();
-			framStringCount.ShowInConsole(true);
+			float floatValue = -123.45f * 10000;
+			Convert.ToInt32(GetReversedByteString(floatValue, 4), 16).ShowInConsole(true);
+		}
+
+		/// <summary>
+		/// 获取浮点型四舍五入后的小端序表示字符串
+		/// </summary>
+		/// <param name="number"></param>
+		/// <returns></returns>
+		private string GetReversedByteString(float number, int bitCount = 3)
+		{
+			int roundNumber = (int)Math.Round(number);
+			int limit = bitCount < 4 ? (1 << (8 * bitCount)) - 1 : int.MaxValue;
+			roundNumber = Math.Min(Math.Max(roundNumber, -limit), limit);
+			byte[] temp = BitConverter.GetBytes(roundNumber);
+			if (!BitConverter.IsLittleEndian)
+				Array.Reverse(temp);
+			byte[] result = new byte[bitCount];
+			Array.Copy(temp, result, bitCount);
+			StringBuilder reultStringBuilder = new StringBuilder();
+			foreach (byte tempByte in result)
+			{
+				reultStringBuilder.Append(tempByte.ToString("X2"));
+			}
+			return reultStringBuilder.ToString();
 		}
 	}
 }
